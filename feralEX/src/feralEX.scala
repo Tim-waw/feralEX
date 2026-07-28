a9ac79abc3cc3846d65c11dce87dd6bbcdbb0715
Tim <tim@witandwhimsey.org>
2026-07-28 17:50:38 -0400

    base for testing

--- feralEX/src/feralEX.scala
+++ feralEX/src/feralEX.scala
@@ -7,28 +7,29 @@ import org.http4s.HttpApp
 import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
 
+import org.http4s.Request
+
 import cats.effect.IO
 import cats.effect.kernel.Resource
 
 import feral.functions.IOAzureHttpFunction
 import feral.functions.facade.InvocationContext
+import org.http4s.Response
 
-object feralEX extends IOAzureHttpFunction {
-  override def buildHttpApp(context: InvocationContext): Resource[IO, HttpApp[IO]] = 
-    val app = HttpRoutes
-      .of[IO] {
-        case GET -> Root / "hello" / name =>
-          Ok(s"Hello, $name.")
-      }
-      .orNotFound
+object feralEXB8 extends IOAzureHttpFunction {
+  override def handler: InvocationContext => Resource[IO,HttpApp[IO]] = {
+    context => {
+      val app = HttpRoutes
+        .of[IO] {
+          case GET -> Root / "api" / "hello" / name =>
+            Ok(s"Hello, $name.")
 
-    Resource.pure(app)
+          case req @ PUT -> Root / "api" =>
+            Ok(req.body)
+        }
+        .orNotFound
+
+      Resource.pure(app)
+    }
+  }
 }
-
-// zip -r functionapp.zip \
-//   host.json \
-//   package.json \
-//   package-lock.json \
-//   src \
-//   node_modules \
-//   -x "local.settings.json" ".vscode/*" ".git/*"
